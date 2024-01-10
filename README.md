2f90ed1fcb9873983558d1dbd932b7e8fe14a35f
manjunathgouda.patil <manjunathgouda.patil@hashouttech.com>
2024-01-10 19:58:09 +0530

    commit

---
+++ README.md
@@ -0,0 +1,52 @@
+#                                                      Youtube-Subscribers Backend Database Project.
+Youtube subscribers backend Api to get the subscribers using specific routes end point made using Node.js , Express.js and MongoDB and Mongoose. By including particular routes end point in the URL, user can access the number of subscribers list,  subscribers name or access subscribers by specific IDs.
+
+Author -  [@Manjunathgouda Patil](https://github.com/manjupatil28)
+
+## **MongoDB Schema Structure**
+
+### Field               -->       Type
+- _id                 -->       ObjectID
+- name                -->       String
+- subscribedChannel   -->       String
+- subscribedDate      -->       Date
+- __v                 -->       Int32
+
+## **Features**
+
+- Access all subscribers from remotely hoisted database on MongoDB
+- Access perticular data from database through a specific IDs
+- Access only subscriber list with names and subscribed Channels
+- Fetch specific data from database via a route
+- Fetch all subscribers from remotely hoisted database
+
+## **Technology/ Languages Used**
+
+- Expressjs
+- MongoDB
+- Mongoose
+- HTML,CSS,JS for Index Page
+
+
+<!-- HOW TO RUN LOCALLY -->
+
+## **How to Run locally**
+- To run this project locally, use the following command in your CLI:
+
+```bash
+git clone 
+```
+- Open your favorite code editor and run locally!
+```bash
+  npm install
+  node index.js
+```
+
+- Open any browser and type in address bar> localhost:3000/
+
+## **Deployment**
+
+
+
+## **NOTE**
+  <p> This is capstone project developed as a part of Full stack web development course with <a href="https://www.almabetter.com">Almabetter</a>.</p>
---
+++ src/app.js
@@ -0,0 +1,59 @@
+
+const express = require('express');
+const app = express()
+const path = require("path")
+const subscriberModel = require('./models/subscribers');
+const staticPath = path.join(__dirname,"../public")
+
+app.use("/",express.static(staticPath))
+
+app.get("/", (req,res)=>{
+    res.json("Hello, Welcome to Youtube subscribers backend app");
+});
+
+// sending GET request to get subscribers list 
+app.get("/subscribers", async(req, res)=>{
+    try{
+// get all the subscribers from the database and exclude the __v field
+        const subscribers= await subscriberModel.find().select("-__v");
+// returns response with list of subscribers & status 200 (OK)        
+        res.status(200).json(subscribers);
+    }catch(err){
+// if error occurs, returns status 400 with error message
+        res.status(400).json({
+            error : "Database Error"
+        });
+    }
+});
+
+
+// sending GET request at the path "/subscribers/name"
+app.get("/subscribers/names", async (req, res)=>{
+
+// To retrieve a list of subscribers    
+    try{   
+        const subscribers= await subscriberModel.find().select("-__v -_id -subscribedDate")
+
+// returns response with list of subscribers & status 200 (OK)
+        res.status(200).json(subscribers);
+    }catch(err){
+
+// if error occurs, returns status 400 with error message
+        res.status(400).json({
+            error : "Invalid name Url"
+        });
+    }
+});
+
+//sending GET request to fetch data as per id
+app.get("/subscribers/:id", async (req, res) => {
+  try{
+    let id =req.params.id;
+    let subscribers= await subscriberModel.findById(id).select("-__v");
+    res.status(200).json(subscribers);
+  }catch(err){
+    res.status(400).json({message: "Invalid Id"});
+  }
+})
+
+module.exports = app;
